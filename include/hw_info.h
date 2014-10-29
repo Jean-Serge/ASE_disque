fdab740cae6632f62f54583b9fa7b6494b344f7b
arctarus44 <arthur.dewarumez@gmail.com>
2014-10-29 22:40:57 +0100

    Ajout de la taille des secteurs.

--- include/hw_info.h
+++ include/hw_info.h
@@ -1,11 +1,12 @@
 #ifndef _H_HW_INFO
 #define _H_HW_INFO
 
-#define HDA_CMDREG      0x3F6    /* registre de commande du disque maitre */
-#define HDA_DATAREGS    0x110    /* registres de données (r,r+1,...r+15) */
-#define HDA_IRQ         14       /* Interruption du disque */
+#define HDA_CMDREG     0x3F6    /* registre de commande du disque maitre */
+#define HDA_DATAREGS   0x110    /* registres de données (r,r+1,...r+15) */
+#define HDA_IRQ        14       /* Interruption du disque */
 #define NB_HW_IRQ       16       /* nombre d'iRQ */
 #define TIMER_IRQ       2        /* Niveau dinterruption de l'horloge */
+#define HDA_SECTORSIZE  32       /* Nombres d'octets constituant un bloc */
 
 #define xHDA_STPS       2        /* nombre de SYSTICK pour changer de secteur */
 #define HDA_STPC        1        /* nombre de SYSTICK pour changer de piste */
