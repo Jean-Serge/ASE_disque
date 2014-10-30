e9f97b06e2a09d968962093c56cc5c5570c6d622
arctarus44 <arthur.dewarumez@gmail.com>
2014-10-30 23:05:16 +0100

    Utilisation des fonction de la question 3. Corrections mineures.

--- frmt.c
+++ frmt.c
@@ -1,26 +1,5 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "include/hardware.h"
 #include "drive.h"
-#include <unistd.h>
-#include <getopt.h>
-#include <string.h>
-
-#define SUCCESS  0
-
-void frmt_sector(unsigned int cyl, unsigned int sec, unsigned char value){
-	int i = 0;
-
-	move_head(cyl, sec);
-
-	for(; i < HDA_SECTORSIZE; i++){
-		MASTERBUFFER[i] = value;
-	}
-	_out_16bits(HDA_DATAREGS, 1);
-	_out(HDA_CMDREG, CMD_WRITE);
-	_sleep(HDA_IRQ);
-}
-
 
 void frmt(){
 	int i;
@@ -29,7 +8,7 @@ void frmt(){
 
 	for(i = 0; i < dinfo->nb_cyl; i++){
 		for(j = 0 ; j < dinfo->nb_sec; j++){
-			frmt_sector(i, j, 0);
+			format_sector(i, j, 1, 0);
 		}
 	}
 }
