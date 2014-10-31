31cadbda26bbb2129ae25e45fab600b36821dfbd
arctarus44 <arthur.dewarumez@gmail.com>
2014-10-31 17:17:08 +0100

    Ajout des fonctions permettant la gestion du mbr. Regroupement des fonctions, define et structure en fonction de leurs utilités.

--- drive.h
+++ drive.h
@@ -3,23 +3,27 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
 #include "include/hardware.h"
 #include "include/hw_info.h"
-
-#
 
 /* Code de retour */
 #define SUCCESS 0
 #define HW_INIT_ERROR 1
 
-/* Macro de deboggage */
-
+/***************************** Macro de deboggage *****************************/
 #define dmps_v (int cyl, int sect){\
 		printf("Lecture cylindre %d secteur %d\t", cyl, sect);	\
 		dmps(cyl, sect);\
 	}\
 
-/* Structure de données utiles */
+/*********** Opération d'I/O d'entier de 2octets dans les registres ***********/
+extern void _out_16bits(unsigned int val, unsigned int addr);
+
+extern unsigned int _in_16bits(unsigned int addr);
+
+
+/********************* Gestion des informations du disque *********************/
 struct disk_info_s{
 	/* TODO : ajouter le fabricant du disque */
 	unsigned int nb_cyl;          /* Le nombre de cylindres du disque */
@@ -27,18 +31,20 @@ struct disk_info_s{
 	unsigned int sec_size;        /* La taille d'un secteur */
 };
 
-extern void mkhd();
-
-extern int is_correct_coordinates(int sec, int cyl);
-
 extern struct disk_info_s *get_disk_info();
 
-extern void _out_16bits(unsigned int val, unsigned int addr);
 
-extern unsigned int _in_16bits(unsigned int addr);
+/***************************  Utilitaire multiples  ***************************/
+extern int is_correct_coordinates(int sec, int cyl);
 
+extern void mkhd();
+
+
+/*************************  Commande de bas niveau  ***************************/
 extern void move_head(unsigned int cyl, unsigned int sec);
 
+
+/***************** Fonction d'écriture et lecture de secteur ******************/
 extern void read_sector(unsigned int cylinder, unsigned int sector,
                         unsigned char *buffer);
 
@@ -47,5 +53,28 @@ extern void write_sector(unsigned int cylinder, unsigned int sector,
 
 extern void format_sector(unsigned int cylinder, unsigned int sector,
                           unsigned int nsector, unsigned int value);
+
+
+/******************************* Gestion du MBR *******************************/
+#define MAX_VOLUME 8
+#define MBR_MAGIC 0xA1E
+enum vol_type{BASE, ANNEXE, OTHER};
+
+struct volume_s{
+	int start_cyl;
+	int start_sect;
+	int nsector;
+	enum vol_type type;
+};
+
+struct mbr_s{
+	int magic;
+	unsigned int nvol;
+	struct volume_s *volume;
+};
+
+extern struct mbr_s *get_mbr();
+
+extern int save_mbr(struct mbr_s mbr);
 
 #endif
