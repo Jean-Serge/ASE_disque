e2824f55bc289f8570a860a8ba6ea5195fdaa040
arctarus44 <arthur.dewarumez@gmail.com>
2014-11-24 23:26:30 +0100

    Écriture d'un programme de test pour la fontion convert_cyl_sec. Affiche le disque sous forme de 8 tableaux de secteur, un par volume pouvant être écrit.
    Le blocs contient OK, si la conversion se retourne un numéro de bloc supérieur ou égale à zéro.
    Sinon KO.

--- test/test_convert_blc.c
+++ test/test_convert_blc.c
@@ -5,21 +5,23 @@
 int main(){
 	int c = 0;
 	int s = 0;
+	int nvol = 0;
 	mkhd();
-	for(c = 0; c < HDA_MAXCYLINDER; c++){
-		printf("Cylinder %d.\n", c);
-		fflush(stdout);
-		for(s = 0; s < HDA_MAXSECTOR; s++){
-			printf("%d : ", s);
-			if(convert_cyl_sec(0, c, s) == -1){
-				printf("\x1b[31mKO\x1b[0m");
+	for(; nvol < 8; nvol++){
+		printf("********************************************************************************\n*                                VOLUME %d                                      *\n********************************************************************************\n", nvol);
+		for(c = 0; c < HDA_MAXCYLINDER; c++){
+			printf("Cylinder %d.\n", c);
+			for(s = 0; s < HDA_MAXSECTOR; s++){
+				printf("%d : ", s);
+				if(convert_cyl_sec(nvol, c, s) == -1){
+					printf("\x1b[31mKO\x1b[0m | ");
+				}
+				else{
+					printf("\x1b[32mOK\x1b[0m | ");
+				}
 			}
-			else{
-				printf("\x1b[32mOK\x1b[0m");
-			}
-			printf(" | ");
+			printf("\n\n");
 		}
-		printf("\n");
 	}
 	return 0;
 }
