0aa119155d7af618f79272efd53949e823403236
arctarus44 <arthur.dewarumez@gmail.com>
2014-11-25 11:59:28 +0100

    Implémentation de la fonction create_ifile() et de la documentation.

--- file.c
+++ file.c
@@ -1,1 +1,5 @@
 #include "file.h"
+
+unsigned int create_ifile(enum file_type_e type){
+	return create_inode(type);
+}
--- file.h
+++ file.h
@@ -1,18 +1,22 @@
 #ifndef _H_FILE
 #define _H_FILE
 
-unsigned int create_ifile(enum file_type_e type);
-int delete_ifile(unsigned int inumber);
+/*
+  Crée un fichier de type type et retourne sont inœud.
+*/
+extern unsigned int create_ifile(enum file_type_e type);
 
-int open_ifile(file_desc_t *fd, unsigned int inumber);
-void close_ifile(file_desc_t *fd);
-void flush_ifile(file_desc_t *fd);
-void seek_ifile(file_desc_t *fd, int r_offset);  /* relatif */
-void seek2_ifile(file_desc_t *fd, int a_offset); /* absolu */
+extern int delete_ifile(unsigned int inumber);
 
-int readc_ifile(file_desc_t *fd);
-int writec_ifile(file_desc_t *fd, char c);
-int read_ifile(file_desc_t *fd, void *buf, unsigned int nbyte);
-int write_ifile(file_desc_t *fd, const void *buf, unsigned int nbyte);
+extern int open_ifile(file_desc_t *fd, unsigned int inumber);
+extern void close_ifile(file_desc_t *fd);
+extern void flush_ifile(file_desc_t *fd);
+extern void seek_ifile(file_desc_t *fd, int r_offset);  /* relatif */
+extern void seek2_ifile(file_desc_t *fd, int a_offset); /* absolu */
+
+extern int readc_ifile(file_desc_t *fd);
+extern int writec_ifile(file_desc_t *fd, char c);
+extern int read_ifile(file_desc_t *fd, void *buf, unsigned int nbyte);
+extern int write_ifile(file_desc_t *fd, const void *buf, unsigned int nbyte);
 
 #endif
