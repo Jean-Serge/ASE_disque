f243056bbc149705d1189ea9b9bd028db9a4e3b3
arctarus44 <arthur.dewarumez@gmail.com>
2014-12-31 02:36:22 +0100

    Fin implémentation file

--- src/file.c
+++ src/file.c
@@ -1,33 +1,41 @@
 #include "file.h"
 
+#define CLOSE_ERROR() fprintf(stderr, "Erreur! Le fichier est fermé.");
+
 
 /****************************** Fonctions utiles ******************************/
 
 unsigned int create_ifile(enum file_type_e type){
-	struct inode_s *inode = NULL;
 	int i = create_inode(type);
-	read_inode(i, inode);
-	printf("inode %p\n", inode);
-	for(i = 0; i < NB_BLOCS; i++){
-		printf("table direct %d : @bloc = %d\n", i, inode->bloc_direct[i]);
-	}
 	return i;
 }
 
+unsigned int delete_ifile(unsigned int inumber){
+	struct inode_s inode;
+	int i = 0;
+	read_inode(inumber, &inode);
+	for(; i < inode.taille; i++){
+		free_bloc(vbloc_of_fbloc(inumber, i, FALSE));
+	}
+	clean_bloc(inumber);
+	return 1;
+}
+
 int open_ifile(file_desc_t *fd, unsigned int inumber){
-	struct inode_s *inode = NULL;
+	struct inode_s inode;
 	int frst_blc = vbloc_of_fbloc(inumber, 0 , FALSE);
 	unsigned char *buf = (unsigned char *)malloc(sizeof(unsigned char)
 	                                             * HDA_SECTORSIZE);
-	read_inode(inumber, inode);
+	read_inode(inumber, &inode);
 
 	if(!fd)
 		fd = (file_desc_t *)malloc(sizeof(file_desc_t));
 
 	fd->inoeud = inumber;
 	fd->cursor = 0;
-	fd->size   = inode->taille;
+	fd->size   = inode.taille;
 	fd->modified = NO;
+	fd->closed = NO;
 	fd->file_buffer = (unsigned char *)
 	                  calloc(sizeof(unsigned char),  HDA_SECTORSIZE);
 	/* Lecture du premier bloc d'adressage direct */
@@ -39,34 +47,80 @@ int open_ifile(file_desc_t *fd, unsigned int inumber){
 	return 0;
 }
 
-void flush_ifile(file_desc_t *fd){
-	if(fd->modified == YES){
-		int i;
-		unsigned char *buf = (unsigned char *)malloc(sizeof(unsigned char *) *
-		                                             HDA_SECTORSIZE);
-		for(i = 0; i < fd->size; i++){
-			memcpy(buf, fd->file_buffer+i, HDA_SECTORSIZE);
-			write_bloc(vol_courant, vbloc_of_fbloc(fd->inoeud, i , FALSE), buf);
-		}
+void print_fd(file_desc_t *fd){
+	printf("Présentation du File Descriptor :\n");
+	printf("\tInœud     : %d\n", fd->inoeud);
+	printf("\tCursor    : %d\n", fd->cursor);
+	printf("\tSize      : %d\n", fd->size);
+	printf("\tModified  : ");
+	switch(fd->modified){
+	case YES:
+		printf("YES\n");
+		break;
+	case NO:
+		printf("NO\n");
+		break;
+	default:
+		printf("ERREUR\n");
+		break;
+	}
+	printf("\tClosed    : ");
+	switch(fd->closed){
+	case YES:
+		printf("YES\n");
+		break;
+	case NO:
+		printf("NO\n");
+		break;
+	default:
+		printf("ERREUR\n");
+		break;
 	}
 }
+
+
+int position_to_fbloc(unsigned int position){
+	return position / HDA_SECTORSIZE;
+}
+
+unsigned int is_closed(file_desc_t *fd){
+	return (fd->closed == YES)?1:0;
+}
+
+void flush_ifile(file_desc_t *fd){
+	if(is_closed(fd)){
+		CLOSE_ERROR();
+		return;
+	}
+	if(fd->modified == YES){
+		int bloc = position_to_fbloc(fd->cursor);
+		unsigned char *buf = (unsigned char *)malloc(sizeof(unsigned char *) *
+		                                             HDA_SECTORSIZE);
+		memcpy(buf, fd->file_buffer, HDA_SECTORSIZE);
+		write_bloc(vol_courant, vbloc_of_fbloc(fd->inoeud, bloc, TRUE), buf);
+	}
+}
+
 
 /**
    Déplacement relatif. Déplacement du curseur de offset octets
  */
 void seek_ifile(file_desc_t *fd, int r_offset){
-	int nw_pos = fd->cursor * HDA_SECTORSIZE + r_offset;
+	if(is_closed(fd)){
+		CLOSE_ERROR();
+		return;
+	}
+	int nw_pos = fd->cursor + r_offset;
 	int nxt_blc, fbloc;
 
-	if(nw_pos / HDA_SECTORSIZE != fd->cursor / HDA_SECTORSIZE){
+	if(position_to_fbloc(nw_pos) != position_to_fbloc(fd->cursor)){;
 		flush_ifile(fd);
 
 		fbloc = nw_pos / HDA_SECTORSIZE;
-		nxt_blc = vbloc_of_fbloc(fd->inoeud, fbloc, FALSE);
+		nxt_blc = vbloc_of_fbloc(fd->inoeud, fbloc, TRUE);
 
-		if(nxt_blc != NULL_BLOC){
+		if(nxt_blc != NULL_BLOC)
 			read_bloc(vol_courant, nxt_blc, fd->file_buffer);
-		}
 		else{
 			fprintf(stderr,
 			        "Impossible de crée un nouveau bloc pour le fichier.\n");
@@ -80,24 +134,67 @@ void seek_ifile(file_desc_t *fd, int r_offset){
    Déplacement absolu. Déplacement du curseur à l'octer offset
  */
 void seek2_ifile(file_desc_t *fd, int a_offset){
-	seek_ifile(fd, a_offset - fd->cursor * HDA_SECTORSIZE);
+	if(is_closed(fd)){
+		CLOSE_ERROR();
+		return;
+	}
+	seek_ifile(fd, a_offset - fd->cursor );
 }
 
 void close_ifile(file_desc_t *fd){
+	if(is_closed(fd)){
+		CLOSE_ERROR();
+		return;
+	}
 	flush_ifile(fd);
+	fd->closed = YES;
 }
 
 int readc_ifile(file_desc_t *fd){
 	int value;
-	if(fd->cursor >= fd->size * HDA_SECTORSIZE)
+	if(is_closed(fd)){
+		CLOSE_ERROR();
 		return READ_EOF;
+	}
+	if(fd->cursor >= fd->size * HDA_SECTORSIZE){
+		return READ_EOF;
+	}
 	value = fd->file_buffer[fd->cursor % HDA_SECTORSIZE];
 
 	seek_ifile(fd, 1);
 	return value;
 }
 
-void write_ifile(file_desc_t fd, char c){
-	fd->file_buffer[fd->cursor] = c;
+int writec_ifile(file_desc_t *fd, unsigned char c){
+	if(is_closed(fd)){
+		CLOSE_ERROR();
+		return 0;
+	}
+	fd->file_buffer[fd->cursor % HDA_SECTORSIZE] = c;
+	fd->modified = YES;
+
 	seek_ifile(fd, 1);
+	return 1;
+}
+
+
+int read_ifile(file_desc_t *fd, unsigned char *buf, unsigned int nbyte){
+	int i = 0;
+	int c;
+	for(i = 0; i < nbyte; i++){
+		c = readc_ifile(fd);
+		if(c == READ_EOF)
+			break;                /* fin du fichier, on stoppe */
+		buf[i] = c;
+	}
+	return i;
+}
+
+
+int write_ifile(file_desc_t *fd, const unsigned char *buf, unsigned int nbyte){
+	int i = 0;
+	for(i = 0; i < nbyte; i++){
+		writec_ifile(fd, buf[i]);
+	}
+	return i;
 }
--- src/file.h
+++ src/file.h
@@ -8,17 +8,6 @@
 
 #define READ_EOF -1
 
-/*
-  Crée un fichier de type type et retourne sont inœud.
-*/
-extern unsigned int create_ifile(enum file_type_e type);
-
-/*
-  Suppression du fichier correspondant à l'inœud inumber.
-  Retourne 0 si le fichier à été supprimé, sinon 1.
-*/
-extern int delete_ifile(unsigned int inumber);
-
 enum modified_e {YES, NO};
 
 struct file_desc_s{
@@ -27,19 +16,47 @@ struct file_desc_s{
 	unsigned int size;
 	unsigned char *file_buffer;
 	enum modified_e modified;
+	enum modified_e closed;
 };
 
 typedef struct file_desc_s file_desc_t;
 
+/*
+  Crée un fichier de type type et retourne sont inœud.
+*/
+/* OK */
+extern unsigned int create_ifile(enum file_type_e type);
 
+/*
+  Suppression du fichier correspondant à l'inœud inumber.
+  Retourne 0 si le fichier à été supprimé, sinon 1.
+*/
+/* OK */
+extern unsigned int delete_ifile(unsigned int inumber);
+/* OK */
+extern int open_ifile(file_desc_t *fd, unsigned int inumber);
+/* OK */
+extern void print_fd(file_desc_t *fd);
+
+/* OK */
 extern void close_ifile(file_desc_t *fd);
+/* OK */
 extern void flush_ifile(file_desc_t *fd);
+/* OK */
 extern void seek_ifile(file_desc_t *fd, int r_offset);  /* relatif */
+/* OK */
 extern void seek2_ifile(file_desc_t *fd, int a_offset); /* absolu */
 
+/* OK */
 extern int readc_ifile(file_desc_t *fd);
-extern int writec_ifile(file_desc_t *fd, char c);
-/* extern int read_ifile(file_desc_t *fd, void *buf, unsigned int nbyte); */
-/* extern int write_ifile(file_desc_t *fd, const void *buf, unsigned int nbyte); */
+
+/* OK */
+extern int writec_ifile(file_desc_t *fd, unsigned char c);
+
+/* OK */
+extern int read_ifile(file_desc_t *fd, unsigned char *buf, unsigned int nbyte);
+
+/* OK */
+extern int write_ifile(file_desc_t *fd, const unsigned char *buf, unsigned int nbyte);
 
 #endif
