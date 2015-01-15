82568e64308a4eaeced9fa07886528881e14ebd9
Jean-Serge <j.monbailly@orange.Fr>
2015-01-15 11:02:53 +0100

    Création du header du shell.

---
+++ src/shell.h
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#define CMD_SIZE 101
+
+extern void display_prompt();
+
+/**
+ * Lit au plus size - 1 caractères sur l'entrée standard et les 
+ * écrit dans cmd, ignore les caractères suivant.
+ * Si une ligne entrée fait plus de size caractères, cette fonction
+ * ignorera les caractères en plus et ne les considérera pas comme une 
+ * autre chaîne.
+ */ 
+extern void my_fgets(char *cmd, unsigned int size);
