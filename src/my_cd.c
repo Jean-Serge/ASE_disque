055d78d993a72bbe9654e2d68951080f57ce8bc3
Jean-Serge <j.monbailly@orange.Fr>
2015-01-15 12:48:48 +0100

    Ajout du squelette permettant d'implémenter le commande cd.

---
+++ src/my_cd.c
@@ -0,0 +1,12 @@
+#include "my_cd.h"
+
+/**
+ * TODO
+ */
+int my_cd(char **args, int argc)
+{
+  printf("Je change de répertoire.\n");
+  printf("Il y a %d arguments.\n", argc);
+
+  return 0;
+}
---
+++ src/my_cd.h
@@ -0,0 +1,12 @@
+#ifndef H_MY_CD
+#define H_MY_CD
+
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * Change le répertoire courant en fonction des paramètres.
+ */
+extern int my_cd(char **args, int argc);
+
+#endif
