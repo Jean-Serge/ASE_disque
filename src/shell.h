768058c104c219ff91ea119e781c415287a426c0
Jean-Serge <j.monbailly@orange.Fr>
2015-01-15 12:17:24 +0100

    Re-création du header du shell finalement nécessaire pour manipuler (entre autres) le répertoire courant.

---
+++ src/shell.h
@@ -0,0 +1,15 @@
+#ifndef H_SHELL
+#define H_SHELL
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "cmd.h"
+#include "my_ls.h"
+
+#define CMD_SIZE 101
+
+char *wd = NULL;
+
+#endif
